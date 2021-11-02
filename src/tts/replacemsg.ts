018e540635dc82b552c4c6397e5edccff6464e6a
asd10384 <65000786+asd10384@users.noreply.github.com>
2021-11-03 00:51:31 +0900

    방식변경 , 변환메세지 추가

--- src/tts/replacemsg.ts
+++ src/tts/replacemsg.ts
@@ -1,14 +1,40 @@
+
+const msgobj: {
+  [key: string]: string
+} = {
+  '\\?': '물음표',
+  '\\!': '느낌표',
+  '\\~': '물결',
+  '\\+': '더하기',
+  '\\-': '빼기',
+  '\\(': '여는소괄호',
+  '\\)': '닫는소괄호',
+  '\\{': '여는중괄호',
+  '\\}': '닫는중괄호',
+  '\\[': '여는대괄호',
+  '\\]': '닫는대괄호',
+  'ㄹㅇ': '리얼',
+  'ㅅㅂ': '시바',
+  'ㄲㅂ': '까비',
+  'ㅎㅇ': '하이',
+  'ㅇㅋ': '오키',
+  'ㄴㅇㅅ': '나이스',
+  'ㅇㅈ': '인정',
+  'ㅅㄱ': '수고',
+  '시발': '야발',
+  '씨발': '야발',
+  '개새끼': '멍새끼',
+  'ㅇㅎ': '아하'
+};
+let msglist: string[] = [];
+for (let i in msgobj) {
+  msglist.push(i);
+}
+let msgreg: RegExp = new RegExp(msglist.join('|'), 'gi');
+
 export default function replacemsg(text: string) {
-  text = text.replace(/\?/gi, '물음표');
-  text = text.replace(/\!/gi, '느낌표');
-  text = text.replace(/\~/gi, '물결');
-  text = text.replace(/\+/gi, '더하기');
-  text = text.replace(/\-/gi, '빼기');
-  text = text.replace(/\(/gi, '여는소괄호');
-  text = text.replace(/\)/gi, '닫는소괄호');
-  text = text.replace(/\{/gi, '여는중괄호');
-  text = text.replace(/\}/gi, '닫는중괄호');
-  text = text.replace(/\[/gi, '여는대괄호');
-  text = text.replace(/\]/gi, '닫는대괄호');
+  text = text.replace(msgreg, (text) => {
+    return (msgobj[text]) ? msgobj[text] : (msgobj['\\'+text]) ? msgobj['\\'+text] : text;
+  });
   return text;
 };
